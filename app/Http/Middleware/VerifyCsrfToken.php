125a26b93847e2b2bd64d6bfef22ee353ddfcaa8
Administrator <1620298436@qq.com>
2022-04-12 16:08:34 +0800

    test

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -6,20 +6,21 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
 {
-    /**
-     * The URIs that should be excluded from CSRF verification.
-     *
-     * @var array
-     */
-    protected $except = [
+	/**
+	 * The URIs that should be excluded from CSRF verification.
+	 *
+	 * @var array
+	 */
+	protected $except = [
 		'api/bra_service/bra_page' ,
-        'bra/bra_cloud/*' ,
-        'bra/bra_service/bra_page' ,
-        'wechat/bra_wechat/pay_callback' ,
-        'wechat/bra_wechat/api' ,
-        'wechat/platform_service/notify' ,
-        'wechat/message_service/callback' ,
-        'update/*' ,
-        'bra/annex/*'
-    ];
+		'bra/bra_cloud/*' ,
+		'pay/callback/index' ,
+		'bra/bra_service/bra_page' ,
+		'wechat/bra_wechat/pay_callback' ,
+		'wechat/bra_wechat/api' ,
+		'wechat/platform_service/notify' ,
+		'wechat/message_service/callback' ,
+		'update/*' ,
+		'bra/annex/*'
+	];
 }
